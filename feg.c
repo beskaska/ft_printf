e07862df542458e13b44ef9ba85f3477956f4ebb
Athena Imelda <aimelda@at-n8.21-school.ru>
2020-02-20 20:47:43 +0300

    20.02.2020 changes

--- feg.c
+++ feg.c
@@ -6,39 +6,35 @@
 /*   By: aimelda <aimelda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 21:34:49 by aimelda           #+#    #+#             */
-/*   Updated: 2020/02/18 21:29:44 by aimelda          ###   ########.fr       */
+/*   Updated: 2020/02/20 20:47:04 by aimelda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	get_length(unsigned long long n, int base)
+static char	*get_number(t_printf *cur, char *txt, int *len)
 {
-	int		len;
+	char	*num;
 
-	len = 1;
-	while (n /= base)
-		len++;
-	return (len);
+	
+	return (num);
 }
 
 int			to_f(t_printf *cur, long double n)
 {
-	int					len;
-	int					tmp;
+	int		len;
+	int		tmp;
+	char	*txt;
 
 	if ((char)cur->content < 0)
 		cur->sign = '-';
-	n = n << 1;
-	len = get_length(nb);
-	tmp = ft_max(len, cur->precision) + cur->sign > 0;
+	txt = get_number(cur, (char*)cur->content, &len);
+	tmp = len + cur->precision + cur->sign > 0;
 	if (!(cur->left_adjusted))
 		while (cur->width > tmp++)
 			ft_putchar(cur->zero);
 	ft_putchar(cur->sign);
-	while (cur->precision > len++)
-		ft_putchar('0');
-	get_string(nb, len - 1, 0);
+	ft_putstr(txt);
 	if (cur->left_adjusted)
 		while (cur->width > tmp++)
 			ft_putchar(cur->zero);
